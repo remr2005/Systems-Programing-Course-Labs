7abae2c358a59f81c22dbabf8728eeb400bb3ce8
UltraUberTurk <morpeh2005ka@gmail.com>
2025-11-29 20:48:01 +0200

    Little fixes

--- 7_lab/stage2.asm
+++ 7_lab/stage2.asm
@@ -7,22 +7,25 @@ ORG 0x7E00
 ; AX = результат (если есть)
 ; Все регистры сохраняются
 ; ----------------------------
-
 start:
     mov ah, 0x00    ; функция BIOS: установить видеорежим
     mov al, 0x03    ; 80x25 текстовый режим, цветной
     int 0x10
 
-    mov si, hello_msg
+    mov si, len_msg
+    call print
+    call strlen
+
+    mov bx, ax          ; сохраняем результат strlen
+    lea si, num_buf     ; адрес буфера для числа
+    call itoa
     call println
 
-    mov ax, 3000
-    call sleep
-    call sleep
-    call sleep
-    call sleep
     mov si, sleep_msg
     call println
+    mov ax, 3000
+    call sleep
+    
     jmp $
 
 ; -----------------------------------------------------
@@ -76,16 +79,31 @@ strlen:
     ret
 
 ; -----------------------------------------------------
-; sleep(AX = миллисек)   BIOS таймер
+; sleep(AX = миллисекунды)   BIOS таймер
 ; -----------------------------------------------------
 sleep:
     push ax
     push bx
-    mov bx, ax
+    push cx
+    push dx
+
+    ; AX = миллисекунды → переводим в микросекунды
+    ; 1 ms = 1000 us
+    mov bx, ax           ; BX = миллисекунды
+    mov ax, 1000
+    mul bx               ; DX:AX = AX*BX = миллисекунды * 1000
+                         ; DX:AX теперь в микросекундах
+
+    ; Теперь DX:AX = количество микросекунд
+    ; BIOS ожидает CX:DX, где DX = младшие 16 бит, CX = старшие 16 бит
+    mov dx, ax           ; младшие 16 бит
+    mov cx, dx           ; старшие 16 бит
+    xor cx, cx           ; на всякий случай старшие = 0 для <65 секунд
     mov ah, 0x86
-    mov cx, 0
-    mov dx, bx
     int 0x15
+
+    pop dx
+    pop cx
     pop bx
     pop ax
     ret
@@ -117,6 +135,57 @@ readline:
     pop bx
     pop ax
     ret
+
+; -----------------------------------------------------
+; itoa: BX = число, SI = адрес буфера, возвращает строку ASCII
+; -----------------------------------------------------
+itoa:
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    mov di, si          ; DI = текущий адрес буфера
+    xor cx, cx          ; счетчик цифр
+
+    cmp bx, 0
+    jne .convert
+    mov byte [di], '0'
+    inc di
+    jmp .done
+
+.convert:
+    mov ax, bx
+
+.next_digit:
+    xor dx, dx
+    mov bx, 10
+    div bx              ; AX / 10 → AX = quot, DX = rem
+    push dx             ; сохраняем остаток (цифру)
+    inc cx
+    mov ax, ax
+    cmp ax, 0
+    jne .next_digit
+
+.reverse:
+    pop dx
+    add dl, '0'         ; цифра -> ASCII
+    mov [di], dl
+    inc di
+    loop .reverse
+
+.done:
+    mov byte [di], 0    ; завершение строки 0
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+
+num_buf times 6 db 0     ; буфер для числа (макс 5 цифр + 0)
+
 
 ; -----------------------------------------------------
 ; readint → AX  (читает целое число из строки)
@@ -158,6 +227,8 @@ readint:
 ; -----------------------------------------------------
 ; Данные
 ; -----------------------------------------------------
-hello_msg db "Boot OK. Stage2 Loaded",0
+hello_msg1 db "Boot OK. ",0
+hello_msg2 db "Stage2 Loaded",0
 sleep_msg db "Sleep 3000ms.",0
+len_msg db "Len of current row is: ",0
 input_buffer times 256 db 0
