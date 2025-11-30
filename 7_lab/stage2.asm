8dbb5a9905053e8dbcd4bf49d3d64ee00bc197f5
UltraUberTurk <morpeh2005ka@gmail.com>
2025-11-30 13:42:39 +0200

    Почти сделал

--- 7_lab/stage2.asm
+++ 7_lab/stage2.asm
@@ -21,15 +21,35 @@ start:
     call itoa
     call println
 
+    ; Таймер
     mov si, sleep_msg
     call println
     mov ax, 3000
     call sleep
 
+    ; Ввод строки
+    mov si, ask_string
+    call print
     call readline
+    mov si, string_result
+    call print
     mov si, input_buffer
     call println
     
+    ;Ввод числа
+    mov si, ask_int
+    call print
+    call readint          ; → AX = число
+    mov bx, ax            ; BX = AX для itoa
+    lea si, num_buf
+    call itoa
+
+    mov si, int_result
+    call print
+    mov si, num_buf
+    call println
+
+
     jmp $
 
 ; -----------------------------------------------------
@@ -247,38 +267,55 @@ num_buf times 6 db 0     ; буфер для числа (макс 5 цифр + 0
 ; readint → AX  (читает целое число из строки)
 ; -----------------------------------------------------
 readint:
-    call readline
-    mov si, input_buffer
-    xor ax, ax            ; ax = результат = 0
+; -----------------------------------------------------
+; readint -> AX
+; Читает строку (через readline) и парсит десятичное число.
+; Возвращает 16-bit результат в AX.
+; Сохраняет регистры: BX,CX,DX,SI (и т.д.)
+; -----------------------------------------------------
+readint:
+    push bx
+    push cx
+    push dx
+    push si
 
-.parse:
-    lodsb
-    or al, al
-    jz .finish            ; 0 → конец строки
+    call readline         ; заполняет input_buffer
+    mov si, input_buffer  ; указатель на начало буфера
+    xor ax, ax            ; результат = 0
 
-    cmp al, '0'
-    jb .finish
-    cmp al, '9'
-    ja .finish
+.parse_loop:
+    mov dl, [si]          ; прочитать байт в DL (не портим AX)
+    inc si
+    or dl, dl
+    jz .parse_done        ; конец строки
 
-    sub al, '0'           ; теперь al = цифра 0..9
+    cmp dl, '0'
+    jb .parse_done
+    cmp dl, '9'
+    ja .parse_done
+
+    sub dl, '0'           ; dl = цифра 0..9
 
     ; ax = ax * 10
     mov bx, ax
+    shl bx, 3             ; bx = ax * 8
     shl ax, 1             ; ax = ax * 2
-    shl ax, 1             ; ax = ax * 4
-    add ax, bx            ; ax = ax * 5
-    add ax, bx            ; ax = ax * 10
+    add ax, bx            ; ax = ax*2 + ax*8 = ax*10
 
-    ; ax += цифра (al)
-    mov bl, al            ; расширить 8-бит в 16-бит
-    xor bh, bh
+    ; ax += dl (цифра)
+    mov bx, 0
+    mov bl, dl
     add ax, bx
 
-    jmp .parse
+    jmp .parse_loop
 
-.finish:
+.parse_done:
+    pop si
+    pop dx
+    pop cx
+    pop bx
     ret
+
 
 ; -----------------------------------------------------
 ; Данные
@@ -287,4 +324,11 @@ hello_msg1 db "Boot OK. ",0
 hello_msg2 db "Stage2 Loaded",0
 sleep_msg db "Sleep 3000ms.",0
 len_msg db "Len of current row is: ",0
+
+ask_string db "Enter the string: ", 0
+string_result db "You entered string: ", 0
+
+ask_int     db "Enter number: ",0
+int_result  db "You entered number: ",0
+
 input_buffer times 256 db 0
